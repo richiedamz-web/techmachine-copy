997eb4e324f037bb0d86a0e4634378e6a63e3e37
richiedamz-web <richiedamz@googlemail.com>
2025-09-16 00:23:45 +0100

    Update slot.js

--- slot.js
+++ slot.js
@@ -1,7 +1,7 @@
 const symbols = [
-  "guitar.jpg", "saxophone.jpg", "trombone.jpg", "piano.jpg",
-  "flute.jpg", "clarinet.jpg", "trumpet.jpg", "violinp.jpg",
-  "drums.jpg", "accordionp.jpg", "balalaika.jpg", "playguitar.jpg"
+  "guitar.jpg", "saxophonem.jpg", "trombone.jpg", "pianom.jpg",
+  "flute.jpg", "clarinet.jpg", "trumpet.jpg", "violinm.jpg",
+  "drums.jpg", "accordionm.jpg", "balalaika.jpg", "playguitar.jpg"
 ];
 
 function spin() {
