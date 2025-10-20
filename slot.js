606762c57b2d7af9a418e0feec8bf1ebe15a8cb5
richiedamz-web <richiedamz@googlemail.com>
2025-10-20 15:54:33 +0100

    Update slot.js

--- slot.js
+++ slot.js
@@ -5,7 +5,7 @@ function getCacheBustedUrl(url) {
 // Symbol list (use the actual filenames of your new sharp images)
 var symbols = [
   "images/animationnew2.jpg",
-  "images/jejoue.jpg",
+  "images/jejouenew.jpg",
   "images/jecodenew2.png",
   "images/jenvoieemails.jpg",
   "images/jenvoiesms.jpg",
